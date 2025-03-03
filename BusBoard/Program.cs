1839e42b4f140c339b0acac13862e54cad352e58
Vani85 <vani.natraj@gmail.com>
2025-03-03 15:24:49 +0000

    Code refactored

--- BusBoard/Program.cs
+++ BusBoard/Program.cs
@@ -6,29 +6,33 @@ namespace BusBoard {
     class BusBoardApp {
         public async static Task Main () {
             try {
-                PrintReport printReport =  new PrintReport();
-                string postCode = "";
-                while(true) {
-                    // Post code to Stop points
-                    Console.WriteLine("Please enter the post code.");
-                    postCode = Console.ReadLine();
-                    if(Utility.validatePostCode(postCode)) {
-                        break;
-                    } else {
-                        Console.WriteLine($"The postcode - {postCode} is invalid.");
-                    }
-                }
+                // Post code to stop points
+                string postCode = GetPostCodeFromUser();                
                 var postCodeInfo = await TFLClient.getPostCodeInformation(postCode);        
                 StopPointsAPIResponse stopPoints = await TFLClient.getStopPointsForthePostCode(postCodeInfo.Result.latitude,postCodeInfo.Result.longitude);                
                 Console.WriteLine($"Count of stop points near the postcode - {postCode} is {stopPoints.stopPoints.Count()}");
 
-                // Stop points to arrival information           
+                // Stop points to arrival information   
+                PrintReport printReport =  new PrintReport();        
                 foreach(var stop in stopPoints.stopPoints) {
                     List<ArrivalsForAStopPoint> arrivals = await TFLClient.getBussesForAGivenStopPoint(stop.naptanId);            
                     printReport.printArrivalInformations(arrivals);
                 }
             } catch(Exception e) {
                 Console.WriteLine(e.Message);
+            }
+        }
+
+        public static string GetPostCodeFromUser() {
+            string PostCode = "";
+            while(true) {
+                Console.WriteLine("Please enter the post code.");
+                PostCode = Console.ReadLine();
+                if(Utility.validatePostCode(PostCode)) {
+                    return PostCode;
+                } else {
+                    Console.WriteLine($"The postcode - {PostCode} is invalid.");
+                }
             }
         }
     }
